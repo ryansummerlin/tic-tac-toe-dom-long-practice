4a5a6e7c32895bf29e9c211cf73f81799fe75067
ryansummerlin <ryansummerlin777@gmail.com>
2023-05-06 12:54:58 -0700

    Finished the game and UI, still have to program button functionality

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,5 +1,15 @@
+
+let board = [];
+for (let i = 0; i < 3; i++) {
+    board.push(Array(3).fill(null));
+}
+
+const gridContainer = document.createElement("div");
+gridContainer.setAttribute("class", "grid-container");
+
 const initializeBoard = function() {
-    const gridContainer = document.querySelector(".grid-container");
+    const buttonContainer = document.querySelector(".button-container");
+    document.body.insertBefore(gridContainer, buttonContainer);
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             let gridItem = document.createElement("div");
@@ -11,4 +21,120 @@ const initializeBoard = function() {
     }
 }
 
+let turnTracker = 0;
+
+const markerArr = ["x", "o"];
+
+const makeMarker = function(turnTracker) {
+    if (turnTracker % 2 === 0) {
+        const xMarker = document.createElement("img");
+        xMarker.setAttribute("src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg");
+        xMarker.setAttribute("class", "marker");
+        return xMarker;
+    } else {
+        const oMarker = document.createElement("img");
+        oMarker.setAttribute("src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg");
+        oMarker.setAttribute("class", "marker");
+        return oMarker;
+    }
+}
+
+const makeMove = function(event) {
+    if (event.target.getAttribute("class") === "marker") {
+        return;
+    }
+
+    let box = event.target;
+    const gridItemChildren = box.children;
+    for (let i = 0; i < gridItemChildren.length; i++) {
+        let el = gridItemChildren[i];
+        if (el.getAttribute("class") === "marker") {
+            return;
+        }
+    }
+
+    const marker = makeMarker(turnTracker);
+    event.target.appendChild(marker);
+
+    let row = box.dataset.row;
+    let col = box.dataset.col;
+    board[row][col] = markerArr[turnTracker % 2];
+
+    turnTracker++;
+
+    if (checkWinner(board)) {
+        let winner = checkWinner(board);
+        const winnerHeader = document.querySelector("h2");
+        winnerHeader.innerText = `Winner: ${winner}`;
+        winnerHeader.style.display = 'block';
+        gridContainer.removeEventListener("click", makeMove);
+    }
+
+}
+
+const checkWinner = function(board) {
+    function checkHorizontalWinner() {
+        for (let i = 0; i < board.length; i++) {
+            let row = board[i];
+            if (row[0] === 'x' && row[1] === 'x' && row[2] === 'x') {
+                return 'X';
+            }
+
+            if (row[0] === 'o' && row[1] === 'o' && row[2] === 'o') {
+                return 'O';
+            }
+        }
+
+        return false;
+    }
+
+    function checkVerticalWinner() {
+        for (let i = 0; i < board.length; i++) {
+            if (board[0][i] === 'x' && board[1][i] === 'x' && board[2][i] === 'x') {
+                return 'X';
+            }
+
+            if (board[0][i] === 'o' && board[1][i] === 'o' && board[2][i] === 'o') {
+                return 'O';
+            }
+        }
+
+        return false;
+    }
+
+    function checkDiagonalWinner() {
+        if (board[0][0] === 'x' && board[1][1] === 'x' && board[2][2] === 'x') {
+            return 'X';
+        }
+
+        if (board[0][0] === 'o' && board[1][1] === 'o' && board[2][2] === 'o') {
+            return 'O';
+        }
+
+        if (board[0][2] === 'x' && board[1][1] === 'x' && board[2][0] === 'x') {
+            return 'X';
+        }
+
+        if (board[0][2] === 'o' && board[1][1] === 'o' && board[2][0] === 'o') {
+            return 'O';
+        }
+
+        return false;
+    }
+
+    if (checkHorizontalWinner()) {
+        return checkHorizontalWinner();
+    } else if (checkVerticalWinner()) {
+        return checkVerticalWinner();
+    } else if (checkDiagonalWinner()) {
+        return checkDiagonalWinner();
+    } else if (turnTracker === 9) {
+        return "None";
+    } else {
+        return false;
+    }
+}
+
+
 document.addEventListener("DOMContentLoaded", initializeBoard);
+gridContainer.addEventListener("click", makeMove);

33f4a7cac562d358d9dea7abf81356f301b3a713
ryansummerlin <ryansummerlin777@gmail.com>
2023-05-05 20:16:55 -0700

    Created the html and css layout for the game

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,1 +1,14 @@
-// Your code here
+const initializeBoard = function() {
+    const gridContainer = document.querySelector(".grid-container");
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let gridItem = document.createElement("div");
+            gridItem.setAttribute("class", "grid-item");
+            gridItem.setAttribute("data-row", i);
+            gridItem.setAttribute("data-col", j);
+            gridContainer.appendChild(gridItem);
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", initializeBoard);
